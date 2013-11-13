4b33134ab36d5f9d33ec66928e0ee46d443ae3d4
= <jaemin.woo@nhn.com>
2013-11-13 10:27:51 +0900

    [MFW-30] Increase version number from 0.0.1-SNAPSHOT to 0.1

--- UtilSetInstrumentationTest/src/com/navercorp/utilsettest/introduction/Introduction.java
+++ UtilSetInstrumentationTest/src/com/navercorp/utilsettest/introduction/Introduction.java
@@ -9,36 +9,42 @@ public enum Introduction {
 	CipherUtilsTestCase_testCipher() {
 		@Override
 		public String getIntroduction() {
-			return "This method simulates simple Cipher";
+			return "CipherUtils provides simple AES encryption.\n" +
+					"So this test shows the ciphertext by applying AES encryption utility function on user entered plaintext." +
+					"Short after the encryption, you can verify the encrypted string can be restored to the exactly same string as the original one by decryption utility function.";
 		}
 	},
 	
 	VolumeUpDownTestCase_testVolumeUpAndDown() {
 		@Override
 		public String getIntroduction() {
-			return "This method simulates simple volume up down test";
+			return "This method manipulates media volume.\n" +
+					"Three times of volume up and then the same number of volume down will be done.";
 		}
 	},
 	
 	KeyboardUtilsTestCase_testKeyboard() {
 		@Override
 		public String getIntroduction() {
-			return "This method simulates simeple keyboard test";
-		}
+			return "This method shows a software keyboard being shown, hid, and toggled.\n" +
+					"\n\n*** Should you not see the keyboard appear, when this being run on emulator, uncheck Hardware keyboard present option on your emulator property in AVD Manager. " +
+					"This will make it work ***";
+		}//  
 	},
 	
 	NetworkListenerTestCase_showIntroductionDialog() {
 		@Override
 		public String getIntroduction() {
-			return "This method simulates simple network listener";
+			return "When network state changes, for example, from 3G/4G to WiFi or in the opposite case, INetworkStateChangedListener will be notified if registered.\n" +
+					"This shows network state change by turning on and off the WiFi Adapter.\n" +
+					"If USIM is not inserted or no Access Point is near, this test shows nothing more than changing WiFi state.";
 		}
-		
 	}, 
 	
 	ScreenUtilsTestCase_testSetBrightness() {
 		@Override
 		public String getIntroduction() {
-			return "Testing ScreenUtilsTestCase...";
+			return "This test makes screen the darkest and then the brightest\n";
 		}
 		
 	},
@@ -46,15 +52,18 @@ public enum Introduction {
 	StringUtils_testHighCompressionRatio() {
 		@Override
 		public String getIntroduction() {
-			return "Testing StringCompression showing high ratio";
+			return "This test simulates string compression util.\n" +
+					"This is good example of how efficiently recurring characters can be compressed, " +
+					"this is not usual case though.\n" +
+					"Following this test, an inefficient test will be executed.\n";
 		}
-		
 	},
 	
 	StringUtils_testLowCompressionRatio() {
 		@Override
 		public String getIntroduction() {
-			return "Testing StringCompression showing low ratio";
+			return "Followed by efficient test previously done, " +
+					"this test demonstrates how disordered characters serverely affects on compression ratio";
 		}
 	}
 	;
@@ -67,7 +76,7 @@ public enum Introduction {
 		try {
 			Thread.sleep(500);
 			idc.show();
-			Thread.sleep(3000);
+			Thread.sleep(5000);
 			idc.dismiss();
 			Thread.sleep(500);
 		} catch (InterruptedException e) {
