4382c22cb27f1f2b303008dc0d2a27d453a38ca0
Sanghyuk Jung <sanghyuk.jung@nhn.com>
2013-11-14 16:36:57 +0900

    [issue-16]Add VolumUtilsTest

---
+++ UtilSet/test/com/navercorp/utilset/audio/VolumeUtilsTest.java
@@ -0,0 +1,76 @@
+package com.navercorp.utilset.audio;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowLog;
+
+import android.content.Context;
+
+/**
+ * @author sanghyuk.jung
+ *
+ */
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest=Config.NONE)
+public class VolumeUtilsTest {
+	private Context context;
+
+	@Before
+	public void setUp() {
+		ShadowLog.stream = System.out;
+		this.context = Robolectric.application;
+	}
+	
+	@Test
+	public void shouldSetVolume(){
+		VolumeUtils.setVolume(context, 3);
+		int volume = VolumeUtils.getCurrentVolume(context);
+		assertThat(volume, is(3));
+	}
+	
+	@Test
+	public void shouldIncreaseVolume(){
+		VolumeUtils.setVolume(context, 3);
+		VolumeUtils.increaseVolume(context);
+		int volume = VolumeUtils.getCurrentVolume(context);
+		assertThat(volume, is(4));
+	}
+	
+	@Test
+	public void shouldIncreaseVolumeWithLevel(){
+		VolumeUtils.setVolumeWithLevel(context, 3);
+		VolumeUtils.increaseVolumeWithLevel(context);
+		int volume = VolumeUtils.getCurrentVolume(context);
+		assertThat(volume, is(4));
+	}
+
+	@Test
+	public void shouldDecreaseVolume(){
+		VolumeUtils.setVolume(context, 3);
+		VolumeUtils.decreaseVolume(context);
+		int volume = VolumeUtils.getCurrentVolume(context);
+		assertThat(volume, is(2));
+	}
+	
+	@Test
+	public void shouldDecreaseVolumeWithLevel(){
+		VolumeUtils.setVolumeWithLevel(context, 3);
+		VolumeUtils.decreaseVolumeWithLevel(context);
+		int volume = VolumeUtils.getCurrentVolume(context);
+		assertThat(volume, is(2));
+	}
+
+	@Test
+	public void shouldGetMaxiumVolume(){
+		int maxVolume = VolumeUtils.getMaximumVolume(context);
+		int maxVolumeInRobolectric = 15;
+		assertThat(maxVolume, is(maxVolumeInRobolectric));	
+	}
+}
