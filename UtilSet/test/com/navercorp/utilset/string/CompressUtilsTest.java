c2b85a67f6b1a31e552d6a5035ddb3316d41e524
jaemin.woo <jaemin.woo@nhn.com>
2013-11-15 17:53:03 +0900

    Add and fix Robolectric based tests

--- UtilSet/test/com/navercorp/utilset/string/CompressUtilsTest.java
+++ UtilSet/test/com/navercorp/utilset/string/CompressUtilsTest.java
@@ -1,7 +1,7 @@
 package com.navercorp.utilset.string;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
---
+++ UtilSet/test/com/navercorp/utilset/system/SystemUtilsTest.java
@@ -0,0 +1,30 @@
+package com.navercorp.utilset.system;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowLog;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+/**
+*
+* @author jaemin.woo
+* 
+*/
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public class SystemUtilsTest {
+	@Before
+	public void setUp() {
+		ShadowLog.stream = System.out;
+	}
+	
+	@Test
+	public void shouldReturnAtLeastOneAsProcessorNumbers() {
+		assertThat(SystemUtils.getProcessorNumbers(), is(greaterThanOrEqualTo(1)));
+	}
+}
