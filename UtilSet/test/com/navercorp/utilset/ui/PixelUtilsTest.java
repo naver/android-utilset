b071324513fa292f328308295444c4f2166a33d7
Sanghyuk Jung <sanghyuk.jung@nhn.com>
2013-11-19 15:25:33 +0900

    [issue-30] Add tests for PixelUtils

---
+++ UtilSet/test/com/navercorp/utilset/ui/PixelUtilsTest.java
@@ -0,0 +1,44 @@
+package com.navercorp.utilset.ui;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowLog;
+
+import android.content.Context;
+
+/**
+*
+* @author sanghyuk.jung
+*/
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public class PixelUtilsTest {
+	private Context context;
+
+	@Before
+	public void setUp() {
+		ShadowLog.stream = System.out;
+		this.context = Robolectric.application;
+	}
+
+	@Test
+	public void shouldGetDpFromPixel(){
+		Robolectric.setDisplayMetricsDensity(1.5f);
+		int dp = PixelUtils.getDpFromPixel(context, 50);
+		assertThat(dp, is(33));
+	}
+	
+	@Test
+	public void shouldPixelDpFromDp(){
+		Robolectric.setDisplayMetricsDensity(1.5f);
+		int pixel = PixelUtils.getPixelFromDP(context, 33);
+		assertThat(pixel, is(50));
+	}
+}
