4ed3743e3d4bfa157b99d763990f8a800528cd1e
Sanghyuk Jung <sanghyuk.jung@nhn.com>
2013-11-19 19:38:37 +0900

    [issue-34] Add test for ActivityUtils

--- UtilSet/test/com/navercorp/utilset/ui/ActivityUtilsTest.java
+++ UtilSet/test/com/navercorp/utilset/ui/ActivityUtilsTest.java
@@ -1,6 +1,6 @@
 package com.navercorp.utilset.ui;
 
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
@@ -19,6 +19,7 @@ import org.robolectric.util.ActivityController;
 
 import android.app.Activity;
 import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
 import android.app.ActivityManager.RunningTaskInfo;
 import android.content.ComponentName;
 import android.content.Context;
@@ -36,6 +37,7 @@ public class ActivityUtilsTest {
 	private Context context;
 	private static final String NOT_INSTALLED_PACKAGE = "seventh.son.of.a.seventh.son";
 	private static final String INSTALLED_PACKAGE = "com.navercorp.utilset";
+	private static final String TEST_ACTIVITY = "TestActivity";
 
 	@Before
 	public void setUp() {
@@ -44,13 +46,13 @@ public class ActivityUtilsTest {
 	}
 
 	@Test
-	public void shouldNotReturnTrueForNotInstalledPackage() {
+	public void shouldBeFalseForNotInstalledPackage() {
 		boolean installed = ActivityUtils.isPackageInstalled(context, NOT_INSTALLED_PACKAGE);
 		assertThat(installed, is(false));
 	}
 
 	@Test
-	public void shouldReturnTrueForInstalledPackage() {
+	public void shouldBeTrueForInstalledPackage() {
 		addInstalledPackage(INSTALLED_PACKAGE);
 		boolean installed = ActivityUtils.isPackageInstalled(context, INSTALLED_PACKAGE);
 		assertThat(installed, is(true));
@@ -59,9 +61,7 @@ public class ActivityUtilsTest {
 	@Test
 	public void shouldAddKeepScreenOnFlag() {
 		TestActivity activity = createActivity(TestActivity.class);
-		
 		ScreenUtils.setScreenOn(activity);
-
 		boolean flag = getFlag(activity, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		assertThat(flag, is(true));
 	}
@@ -69,20 +69,62 @@ public class ActivityUtilsTest {
 	@Test
 	public void shouldClearKeepScreenOnFlag() {
 		TestActivity activity = createActivity(TestActivity.class);
-		
 		ScreenUtils.clearScreenOn(activity);
-
 		boolean flag = getFlag(activity, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		assertThat(flag, is(false));
 	}
 
 	@Test
-	public void shouldGetBaseActivityPakcageName() {
-		setBaseActivityPackage(INSTALLED_PACKAGE);
+	public void shouldGetBaseActivityPackageName() {
+		createRunningTaskInfo().baseActivity = new ComponentName(INSTALLED_PACKAGE, "");
 		String packageName = ActivityUtils.getBaseActivityPackageName(context);
 		assertThat(packageName, is(INSTALLED_PACKAGE));
 	}
+
+	@Test
+	public void shouldGetBaseActivityClassName() {
+		createRunningTaskInfo().baseActivity = new ComponentName(INSTALLED_PACKAGE, TEST_ACTIVITY);
+		String className = ActivityUtils.getBaseActivityClassName(context);
+		assertThat(className, is(TEST_ACTIVITY));
+	}
+
+	@Test
+	public void shouldGetTopActivityPackageName() {
+		createRunningTaskInfo().topActivity = new ComponentName(INSTALLED_PACKAGE, "");
+		String packageName = ActivityUtils.getTopActivityPackageName(context);
+		assertThat(packageName, is(INSTALLED_PACKAGE));
+	}
+
+	@Test
+	public void shouldGetTopActivityClassName() {
+		createRunningTaskInfo().topActivity = new ComponentName(INSTALLED_PACKAGE, TEST_ACTIVITY);
+		String className = ActivityUtils.getTopActivityClassName(context);
+		assertThat(className, is(TEST_ACTIVITY));
+	}
 	
+	@Test
+	public void shouldBeFalseWhenThisApplicationIsNotTop() {
+		createRunningTaskInfo().topActivity = new ComponentName(INSTALLED_PACKAGE, TEST_ACTIVITY);
+		boolean top = ActivityUtils.isTopApplication(context);
+		assertThat(top, is(false));
+	}
+	
+	@Test
+	public void shouldBeTrueWhenThisContextIsForeground(){
+		// android.os.Process.myPid() returns 0 in Robolectric
+		createRunningAppProcessInfo(0);
+		boolean foreground = ActivityUtils.isContextForeground(context);
+		assertThat(foreground, is(true));
+	}
+
+	@Test
+	public void shouldBeFalseWhenThisContextIsNotForeground(){
+		// android.os.Process.myPid() returns 0 in Robolectric
+		createRunningAppProcessInfo(3);
+		boolean foreground = ActivityUtils.isContextForeground(context);
+		assertThat(foreground, is(false));
+	}
+
 	private void addInstalledPackage(String packageName) {
 		RobolectricPackageManager pm = Robolectric.packageManager;
 		PackageInfo packInfo = new PackageInfo();
@@ -90,14 +132,13 @@ public class ActivityUtilsTest {
 		pm.addPackage(packInfo);
 	}
 	
-	private void setBaseActivityPackage(String packageName) {
+	private RunningTaskInfo createRunningTaskInfo() {
 		ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
 		ShadowActivityManager shadowAm = Robolectric.shadowOf(am);
 		RunningTaskInfo info = new RunningTaskInfo();
-		info.baseActivity = new ComponentName(packageName, "test");
 		shadowAm.setTasks(Arrays.asList(info));
+		return info;
 	}
-
 
 	private <T extends Activity> T createActivity(Class<T> activityClass) {
 		ActivityController<T> controller = Robolectric.buildActivity(activityClass);
@@ -110,6 +151,16 @@ public class ActivityUtilsTest {
 		return window.getFlag(flagToMatch);
 	}
 
+	private void createRunningAppProcessInfo(int pid) {
+		ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+		ShadowActivityManager shadowAm = Robolectric.shadowOf(am);
+		
+		RunningAppProcessInfo proc = new RunningAppProcessInfo();
+		proc.importance = RunningAppProcessInfo.IMPORTANCE_FOREGROUND;
+		proc.pid = pid;
+		shadowAm.setProcesses(Arrays.asList(proc));
+	}
+	
 	static class TestActivity extends Activity {
 	}
 }
