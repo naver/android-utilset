d6e3d49bfec31db1f27a123db8cd2516a28f9ba9
jaemin.woo <jaemin.woo@nhn.com>
2014-02-20 19:12:45 +0900

    Update create tag of 1.0.1 version, commence 1.0.2-SNAPSHOT version

--- UtilSet/test/com/navercorp/utilset/system/SystemUtilsTest.java
+++ UtilSet/test/com/navercorp/utilset/system/SystemUtilsTest.java
@@ -1,15 +1,14 @@
 package com.navercorp.utilset.system;
 
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowLog;
-
-import static org.hamcrest.Matchers.*;
-import static org.hamcrest.MatcherAssert.*;
-import static org.junit.Assert.*;
 
 /**
 *
@@ -26,7 +25,6 @@ public class SystemUtilsTest {
 	
 	@Test
 	public void shouldReturnAtLeastOneAsProcessorNumbers() {
-		assertEquals(SystemUtils.getProcessorNumbers(), 4);
-//		assertThat(SystemUtils.getProcessorNumbers(), is(greaterThanOrEqualTo(1)));
+		assertThat(SystemUtils.getProcessorNumbers(), is(greaterThanOrEqualTo(1)));
 	}
 }
